8db66efa51b0f8758b40d3ca5454619ae2f4f227
Rodion <rodion.morozov.01@gmail.com>
2019-01-18 00:12:42 +0700

    Added TPS counter

--- craftersmine.EtherEngine.Core/GameUpdater.cs
+++ craftersmine.EtherEngine.Core/GameUpdater.cs
@@ -10,7 +10,10 @@ namespace craftersmine.EtherEngine.Core
     public sealed class GameUpdater
     {
         private AccurateTimer _gameUpdaterThread;
+        private AccurateTimer _tpsUpdater;
         private int _delayBetweenTicks = 0;
+        private int _ticks = 0;
+        private int _lastTicks = 0;
 
         public int Tickrate { get; private set; }
 
@@ -19,32 +22,32 @@ namespace craftersmine.EtherEngine.Core
             Tickrate = tickrate;
             _delayBetweenTicks = 1000 / tickrate;
             _gameUpdaterThread = new AccurateTimer(_onUpdate, _delayBetweenTicks);
+            _tpsUpdater = new AccurateTimer(new Action(() => { _lastTicks = _ticks; _ticks = 0; Debugging.TPS = _lastTicks; }), 1000);
         }
 
         public void Run()
         {
             Debugging.Log(LogEntryType.Info, "Starting GameUpdater...");
             _gameUpdaterThread.Start();
+            _tpsUpdater.Start();
         }
 
         public void Stop()
         {
             Debugging.Log(LogEntryType.Info, "Stopping GameUpdater...");
             _gameUpdaterThread.Stop();
+            _tpsUpdater.Stop();
         }
-
-        public event EventHandler GameUpdate;
 
         private void _onUpdate()
         {
-            if (Debugging.ShowDrawCallsPerFrameInTitle)
-                Game.GameWnd.Title = Game.DefaultWindowTitle + " | " + GameRendererHelper.DrawCallsPerFrame + " DrawCalls/frame";
             if (SceneManager.CurrentScene != null)
             {
                 SceneManager.CurrentScene.InternalUpdate();
                 for (int obj = 0; obj < SceneManager.CurrentScene.GameObjects.Count; obj++)
                     SceneManager.CurrentScene.GameObjects[obj].InternalUpdate();
             }
+            _ticks++;
         }
     }
 }
