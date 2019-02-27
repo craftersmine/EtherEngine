0bf4803de702ba3bdae5aa0f2bfc8bbb4ff95e95
Rodion <rodion.morozov.01@gmail.com>
2019-02-27 22:27:09 +0700

    Added rotation origin

--- craftersmine.EtherEngine.Core/Transform.cs
+++ craftersmine.EtherEngine.Core/Transform.cs
@@ -16,6 +16,8 @@ namespace craftersmine.EtherEngine.Core
         public int Width { get; set; }
         public int Height { get; set; }
 
+        public int RotationOriginX { get; set; }
+        public int RotationOriginY { get; set; }
         public float RotationAngle { get; set; }
 
         public void Rotate(float angle)
@@ -23,6 +25,12 @@ namespace craftersmine.EtherEngine.Core
             RotationAngle += angle;
             if (RotationAngle >= 360.0f)
                 RotationAngle = RotationAngle - 360.0f;
+        }
+
+        public void ResetRotationOrigin()
+        {
+            RotationOriginX = Width / 2 + RendererX;
+            RotationOriginY = Height / 2 + RendererY;
         }
     }
 }
