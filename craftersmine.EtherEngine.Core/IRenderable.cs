da5144a30febe62d2b02cf14f7fbe3e9ce20daf9
Rodion <rodion.morozov.01@gmail.com>
2019-02-27 10:47:58 +0700

    Added IRenderable

---
+++ craftersmine.EtherEngine.Core/IRenderable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.EtherEngine.Content;
+using craftersmine.EtherEngine.Rendering;
+
+namespace craftersmine.EtherEngine.Core
+{
+    public interface IRenderable
+    {
+        Texture Texture { get; set; }
+        Transform Transform { get; set; }
+
+        void OnRender(GLGDI renderer);
+    }
+}
