8eb48ced37b4b0e77426fd5bb459ea00ece58385
Rodion <rodion.morozov.01@gmail.com>
2019-03-04 09:54:29 +0700

    Added scene audio channel

---
+++ craftersmine.EtherEngine.Core/AudioChannel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.EtherEngine.Content;
+
+namespace craftersmine.EtherEngine.Core
+{
+    public sealed class AudioChannel
+    {
+        /// <summary>
+        /// Gets current audio clip used by this audio channel
+        /// </summary>
+        public AudioClip AudioClip { get; private set; }
+
+        /// <summary>
+        /// Creates new instance of <see cref="AudioChannel"/> with specified audio clip
+        /// </summary>
+        /// <param name="audioClip"></param>
+        public AudioChannel(AudioClip audioClip)
+        {
+            AudioClip = audioClip;
+        }
+
+        /// <summary>
+        /// Plays audio clip
+        /// </summary>
+        public void Play()
+        {
+            if (AudioClip.OggStream.Ready)
+                AudioClip.OggStream.Play();
+        }
+
+        /// <summary>
+        /// Pauses playing audio clip
+        /// </summary>
+        public void Pause()
+        {
+            AudioClip.OggStream.Pause();
+        }
+
+        /// <summary>
+        /// Stops playing audio clip
+        /// </summary>
+        public void Stop()
+        {
+            AudioClip.OggStream.Stop();
+        }
+
+        /// <summary>
+        /// Resumes paused audio clip
+        /// </summary>
+        public void Resume()
+        {
+            AudioClip.OggStream.Resume();
+        }
+    }
+}
