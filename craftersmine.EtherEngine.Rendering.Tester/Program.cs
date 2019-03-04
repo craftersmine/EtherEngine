4c0947ec62c69ef38a1a540af770382a201a9741
Rodion <rodion.morozov.01@gmail.com>
2019-03-04 22:04:03 +0700

    Updated Tester app

--- craftersmine.EtherEngine.Rendering.Tester/Program.cs
+++ craftersmine.EtherEngine.Rendering.Tester/Program.cs
@@ -90,6 +90,7 @@ namespace craftersmine.EtherEngine.Rendering.Tester
             particleSystem.X = 200; particleSystem.Y = 200;
             particleSystem.Collidable = true;
             particleSystem.SetCollsionBox(new CollisionBox(0, 0, 32, 32));
+            particleSystem.OnUpdateAction = new ParticleOnUpdateDelegate(ParticleOnUpdate);
 
             for (int x = 0; x < SceneCamera.FrameWidth / 2 / 32 + 1; x++)
                 for (int y = 0; y < SceneCamera.FrameHeight / 2 / 32 + 1; y++)
@@ -114,6 +115,12 @@ namespace craftersmine.EtherEngine.Rendering.Tester
             //particleSystem.Emit();
             audioclip = Program.contentManager.LoadAudioClip("TestAudioClip");
             CreateAudioChannel("aud", audioclip);
+        }
+
+        private void ParticleOnUpdate(Particle particle)
+        {
+            float lifetimeFac = (float)particle.CurrentLifetime / (float)particle.Lifetime;
+            particle.ObjectTransparency = 1.0f * lifetimeFac;
         }
 
         public override void OnUpdate()
