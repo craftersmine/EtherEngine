e815188cced3eea8a616343ac47a5e383038134f
Rodion <rodion.morozov.01@gmail.com>
2019-03-18 16:24:57 +0700

    Added Tile

---
+++ craftersmine.EtherEngine.Core/Objects/Tile.cs
@@ -0,0 +1,15 @@
+﻿namespace craftersmine.EtherEngine.Core
+{
+    public class Tile : GameObject
+    {
+        public int TilesetX { get; set; }
+        public int TilesetY { get; set; }
+        public string TileId { get; set; }
+
+        public Tile(int tilesetX, int tilesetY)
+        {
+            TilesetX = tilesetX;
+            TilesetY = tilesetY;
+        }
+    }
+}
