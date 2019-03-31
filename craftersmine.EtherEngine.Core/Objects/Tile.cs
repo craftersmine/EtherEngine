c238e7fee4d9792a85ceb3fd0c07354e79a8a76c
Rodion <rodion.morozov.01@gmail.com>
2019-03-31 15:35:58 +0700

    Updated Tileset and Tile

--- craftersmine.EtherEngine.Core/Objects/Tile.cs
+++ craftersmine.EtherEngine.Core/Objects/Tile.cs
@@ -1,15 +1,29 @@
 ﻿namespace craftersmine.EtherEngine.Core
 {
+    /// <summary>
+    /// Represents a world tile
+    /// </summary>
     public class Tile : GameObject
     {
+        /// <summary>
+        /// Gets or sets tile position along X axis relative to <see cref="Tileset"/>
+        /// </summary>
         public int TilesetX { get; set; }
+        /// <summary>
+        /// Gets or sets tile position along Y axis relative to <see cref="Tileset"/>
+        /// </summary>
         public int TilesetY { get; set; }
+        /// <summary>
+        /// Gets or sets internal Tile id. May be useful for specific actions
+        /// </summary>
         public string TileId { get; set; }
 
-        public Tile(int tilesetX, int tilesetY)
+        /// <summary>
+        /// Creates a new instance of <see cref="Tile"/>
+        /// </summary>
+        public Tile()
         {
-            TilesetX = tilesetX;
-            TilesetY = tilesetY;
+
         }
     }
 }
--- craftersmine.EtherEngine.Core/Objects/Tileset.cs
+++ craftersmine.EtherEngine.Core/Objects/Tileset.cs
@@ -5,64 +5,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using craftersmine.EtherEngine.Content;
+using craftersmine.EtherEngine.Utilities;
 
 namespace craftersmine.EtherEngine.Core
 {
+    /// <summary>
+    /// Represents a world tileset. This class cannot be inherited
+    /// </summary>
     public sealed class Tileset : GameObject
     {
         private List<Tile> _tiles = new List<Tile>();
-        private Dictionary<string, Tile> _tilesPresets = new Dictionary<string, Tile>();
 
-        public TextureSet TilesTextures { get; private set; }
+        /// <summary>
+        /// Gets current tiles width
+        /// </summary>
         public int TileWidth { get; private set; }
+        /// <summary>
+        /// Gets current tiles height
+        /// </summary>
         public int TileHeight { get; private set; }
+        /// <summary>
+        /// Gets or sets <see cref="Tile"/> OnUpdate action
+        /// </summary>
         public TileOnUpdateAction TileOnUpdateAction { get; set; }
 
-        public Tileset(TextureSet tilesTextures, int tileWidth, int tileHeight)
+        /// <summary>
+        /// Creates a new instance of <see cref="Tileset"/> with specific tiles size and global tiles OnUpdate action
+        /// </summary>
+        /// <param name="tileWidth">Width of adding tiles</param>
+        /// <param name="tileHeight">Height of adding tiles</param>
+        /// <param name="onTileUpdateAction">Tiles global OnUpdate action</param>
+        public Tileset(int tileWidth, int tileHeight, TileOnUpdateAction onTileUpdateAction)
         {
-            TilesTextures = tilesTextures;
-        }
-
-        public override void OnUpdate()
-        {
-            foreach (Tile tile in _tiles)
-            {
-                TileOnUpdateAction?.Invoke(tile);
-            }
-        }
-
-        public Tile CheckTile(int x, int y)
-        {
-            throw new NotImplementedException("[WIP] CheckTile is not implemented yet");
-        }
-        
-        public void AddTile(string tilePresetId, int x, int y)
-        {
-            try
-            {
-                Tile clonedTile = _tilesPresets[tilePresetId].Clone<Tile>();
-                clonedTile.TilesetX = x;
-                clonedTile.TilesetY = y;
-                _tiles.Add(clonedTile);
-            }
-            catch { }
+            TileWidth = tileWidth;
+            TileHeight = tileHeight;
+            TileOnUpdateAction = onTileUpdateAction;
         }
 
         /// <summary>
-        /// Tries to add preset of tile and returns true if success, otherwise false
+        /// Occurs when <see cref="Tileset"/> being updated
         /// </summary>
-        /// <param name="tile"></param>
-        /// <returns></returns>
-        public bool AddTilePreset(Tile tile)
+        public override void OnUpdate()
         {
-            if (!_tilesPresets.ContainsKey(tile.TileId))
+            for (int i = 0; i < _tiles.Count; i++)
             {
-                _tilesPresets.Add(tile.TileId, tile);
-                return true;
+                _tiles[i].OnUpdate();
+                TileOnUpdateAction?.Invoke(_tiles[i]);
             }
-            else return false;
+        }
+
+        /// <summary>
+        /// Checks for a tile under specific world coordinates and returns <see cref="Tile"/> if it exists or <code>null</code> if it doesn't exist at this coordinates
+        /// </summary>
+        /// <param name="x">Coordinate along X axis to check <see cref="Tile"/></param>
+        /// <param name="y">Coordinate along Y axis to check <see cref="Tile"/></param>
+        /// <returns></returns>
+        public Tile CheckTile(int x, int y)
+        {
+            int tileX = x / TileWidth;
+            int tileY = y / TileHeight;
+
+            foreach (var tile in _tiles)
+            {
+                if (tile.X == tileX && tile.Y == tileY)
+                    return tile;
+                else continue;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Adds specific <see cref="Tile"/> at specified location in tileset
+        /// </summary>
+        /// <param name="tile">Tile to add</param>
+        /// <param name="x">Coordinate along X axis (all tiles have coordinates like 1,1; 1,2; 3,10 etc.)</param>
+        /// <param name="y">Coordinate along Y axis (all tiles have coordinates like 1,1; 1,2; 3,10 etc.)</param>
+        public void AddTile(Tile tile, int x, int y)
+        {
+            try
+            {
+                tile.TilesetX = x;
+                tile.TilesetY = y;
+                _tiles.Add(tile);
+            }
+            catch (Exception ex)
+            {
+                Debugging.Log(LogEntryType.Warning, "Unable to add tile " + tile.TileId + " @" + x + "," + y);
+                Debugging.LogException(LogEntryType.Warning, ex);
+            }
         }
     }
 
+    /// <summary>
+    /// Represents <see cref="Tile"/> OnUpdate behaviour
+    /// </summary>
+    /// <param name="updatingTile"></param>
     public delegate void TileOnUpdateAction(Tile updatingTile);
 }
