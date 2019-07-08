d4d441cf1393f06fbd0fc370eab2975221eae183
Rodion <rodion.morozov.01@gmail.com>
2019-07-08 09:46:08 +0700

    Added System Environment variables class

---
+++ craftersmine.EtherEngine.Core/Core/SystemEnvironment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.EtherEngine.Core
+{
+    /// <summary>
+    /// Contains static system-specific variables and methods
+    /// </summary>
+    public static class SystemEnvironment
+    {
+        /// <summary>
+        /// %TEMP% directory
+        /// </summary>
+        public static string TemporaryDirectory { get { return Environment.GetEnvironmentVariable("temp"); } }
+        /// <summary>
+        /// AppData/Roaming user directory
+        /// </summary>
+        public static string AppDataDirectory { get { return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData); } }
+        /// <summary>
+        /// AppData/Local user directory
+        /// </summary>
+        public static string LocalAppDataDirectory { get { return Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData); } }
+        /// <summary>
+        /// User documents directory
+        /// </summary>
+        public static string MyDocumentsDirectory { get { return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); } }
+        /// <summary>
+        /// Current game launch arguments
+        /// </summary>
+        public static string[] LaunchArguments { get { return Environment.GetCommandLineArgs(); } }
+    }
+}
