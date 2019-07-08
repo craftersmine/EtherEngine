e161047745b2ab2f7b36500d3b3741a4378a551e
Rodion <rodion.morozov.01@gmail.com>
2019-07-08 09:50:41 +0700

    Added gamepad analog controls enumerators

---
+++ craftersmine.EtherEngine.Core/Input/GamepadAnalogs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XInput = SharpDX.XInput;
+
+namespace craftersmine.EtherEngine.Input
+{
+    /// <summary>
+    /// Contains gamepad triggers
+    /// </summary>
+    public enum GamepadTriggers
+    {
+        /// <summary>
+        /// Left trigger - LT
+        /// </summary>
+        LeftTrigger,
+        /// <summary>
+        /// Right trigger - RT
+        /// </summary>
+        RightTrigger
+    }
+
+    /// <summary>
+    /// Contains gamepad thumbsticks
+    /// </summary>
+    public enum GamepadSticks
+    {
+        /// <summary>
+        /// Left thumbstick - LS
+        /// </summary>
+        LeftStick = XInput.GamepadButtonFlags.LeftThumb,
+        /// <summary>
+        /// Right thumbstick - RS
+        /// </summary>
+        RightStick = XInput.GamepadButtonFlags.RightThumb
+    }
+}
