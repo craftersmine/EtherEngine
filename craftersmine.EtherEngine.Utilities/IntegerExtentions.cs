6882ef4fcd47d3a641076164165f83e99b78a10d
Rodion <rodion.morozov.01@gmail.com>
2019-07-08 09:58:12 +0700

    Added documentation comments

--- craftersmine.EtherEngine.Utilities/IntegerExtentions.cs
+++ craftersmine.EtherEngine.Utilities/IntegerExtentions.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace craftersmine.EtherEngine.Utilities
 {
+    /// <summary>
+    /// Contains static methods for integer extentions
+    /// </summary>
     public static class IntegerExtentions
     {
+        /// <summary>
+        /// Returns string with hexademical value of integer
+        /// </summary>
+        /// <param name="value">Integer</param>
+        /// <returns></returns>
         public static string ToHexademicalString(this int value)
         {
             return value.ToString("X");
         }
 
+        /// <summary>
+        /// Returns string with hexademical value of integer with 0x prefix
+        /// </summary>
+        /// <param name="value">Integer</param>
+        /// <returns></returns>
         public static string ToHexademicalStringWithPrefix(this int value)
         {
             return "0x" + value.ToHexademicalString();
