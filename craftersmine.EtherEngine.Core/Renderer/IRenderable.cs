c29cf907b513cd6e2cdb1af3dad4e9cc17ec4a31
Rodion <rodion.morozov.01@gmail.com>
2019-07-08 09:56:34 +0700

    Updated IRenderable interface

--- craftersmine.EtherEngine.Core/Renderer/IRenderable.cs
+++ craftersmine.EtherEngine.Core/Renderer/IRenderable.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpDX.Direct2D1;
 
 namespace craftersmine.EtherEngine.Renderer
 {
     interface IRenderable
     {
-        void OnRender();
+        void OnRender(RenderTarget renderTarget);
     }
 }
