8795859ed7e0a89e7fee3ec7eaf6f9913c476853
Rodion <rodion.morozov.01@gmail.com>
2019-07-08 09:56:05 +0700

    Added keyboard key state

---
+++ craftersmine.EtherEngine.Core/Input/KeyState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.EtherEngine.Input
+{
+    /// <summary>
+    /// Represents keyboard key state
+    /// </summary>
+    public struct KeyState
+    {
+        /// <summary>
+        /// Gets keyboard key
+        /// </summary>
+        public Key Key { get; internal set; }
+        /// <summary>
+        /// Gets true if key pressed, otherwise false
+        /// </summary>
+        public bool IsPressed { get; internal set; }
+        /// <summary>
+        /// Gets true if key released, otherwise false
+        /// </summary>
+        public bool IsReleased { get; internal set; }
+    }
+}
