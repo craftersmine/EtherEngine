b2726a8c04ed6dd82f92fe643bfb223829a83920
Rodion <rodion.morozov.01@gmail.com>
2019-07-19 19:18:07 +0700

    Added blend mode enumeration

---
+++ craftersmine.EtherEngine.Core/Core/BlendMode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.EtherEngine.Core
+{
+    /// <summary>
+    /// Contains all possible pixel blend options
+    /// </summary>
+    public enum BlendMode
+    {
+        None,
+        Add,
+        Multiply,
+        Light,
+        Subtract,
+        Divide
+    }
+}
