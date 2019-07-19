6e7506f08fff8e8dcab0e176218ab2e7141a8a15
Rodion <rodion.morozov.01@gmail.com>
2019-07-19 19:30:11 +0700

    Added documentation

--- craftersmine.EtherEngine.Core/Renderer/IRenderable.cs
+++ craftersmine.EtherEngine.Core/Renderer/IRenderable.cs
@@ -7,8 +7,15 @@ using SharpDX.Direct2D1;
 
 namespace craftersmine.EtherEngine.Renderer
 {
+    /// <summary>
+    /// Represents a renderable object
+    /// </summary>
     interface IRenderable
     {
+        /// <summary>
+        /// Called when renderer performs frame draw
+        /// </summary>
+        /// <param name="renderFrame">Render frame where you can draw</param>
         void OnRender(RenderFrame renderFrame);
     }
 }
