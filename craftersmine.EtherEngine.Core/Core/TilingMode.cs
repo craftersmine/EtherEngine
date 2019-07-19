11de66314f26d4cb0ea637d97a7e72f6c669494d
Rodion <rodion.morozov.01@gmail.com>
2019-07-19 19:20:51 +0700

    Added Tiling mode enumeration

---
+++ craftersmine.EtherEngine.Core/Core/TilingMode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.EtherEngine.Core
+{
+    /// <summary>
+    /// Contains all possible texture tiling modes
+    /// </summary>
+    public enum TilingMode
+    {
+        /// <summary>
+        /// Stretches texture on whole bounding box
+        /// </summary>
+        Stretch,
+        /// <summary>
+        /// Centers texture within bounding box
+        /// </summary>
+        Center,
+        /// <summary>
+        /// Tiles texture within bounding box
+        /// </summary>
+        Tile
+    }
+}
