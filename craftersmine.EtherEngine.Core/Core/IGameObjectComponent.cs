6e4cc4be1fc327e1969bbc68b0147c71d421f7a4
Rodion <rodion.morozov.01@gmail.com>
2019-07-19 19:17:11 +0700

    Added GameObjectComponent interface

---
+++ craftersmine.EtherEngine.Core/Core/IGameObjectComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.EtherEngine.Core
+{
+    /// <summary>
+    /// Represents basic game object component
+    /// </summary>
+    public interface IGameObjectComponent
+    {
+        bool IsCreated { get; }
+
+        /// <summary>
+        /// Gets parent game object that contains this component
+        /// </summary>
+        GameObject ParentGameObject { get; }
+
+        /// <summary>
+        /// Calls on component creation
+        /// </summary>
+        /// <param name="gameObject">Game object that initializes this component</param>
+        void OnCreate(GameObject gameObject);
+
+        /// <summary>
+        /// Calls on component update
+        /// </summary>
+        void OnUpdate();
+
+        /// <summary>
+        /// Calls on component fixed update
+        /// </summary>
+        void OnFixedUpdate();
+    }
+}
